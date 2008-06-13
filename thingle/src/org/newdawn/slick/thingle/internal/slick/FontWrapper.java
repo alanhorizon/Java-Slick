a003eae82e8aad889f7bb606c146f9a22d553d8d
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2008-06-13 18:43:45 +0000

    Javadocced slick SPI impl

--- thingle/src/org/newdawn/slick/thingle/internal/slick/FontWrapper.java
+++ thingle/src/org/newdawn/slick/thingle/internal/slick/FontWrapper.java
@@ -3,21 +3,43 @@ package org.newdawn.slick.thingle.internal.slick;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.thingle.spi.ThinletFont;
 
+/**
+ * A wrapper to make a Slick font look like a Thinlet font
+ * 
+ * @author kevin
+ */
 public class FontWrapper implements ThinletFont {
+	/** The font wrapped */
 	private Font font;
 	
+	/**
+	 * Create a new font wrapped
+	 * 
+	 * @param font The font to be wrapped
+	 */
 	FontWrapper(Font font) {
 		this.font = font;
 	}
 	
+	/**
+	 * @see org.newdawn.slick.thingle.spi.ThinletFont#getLineHeight()
+	 */
 	public int getLineHeight() {
 		return font.getLineHeight();
 	}
 
+	/**
+	 * @see org.newdawn.slick.thingle.spi.ThinletFont#getWidth(java.lang.String)
+	 */
 	public int getWidth(String str) {
 		return font.getWidth(str);
 	}
 
+	/**
+	 * Get the wrapped font
+	 * 
+	 * @return The slick font that is wrapped up
+	 */
 	public Font getSlickFont() {
 		return font;
 	}
--- thingle/src/org/newdawn/slick/thingle/internal/slick/ImageBufferWrapper.java
+++ thingle/src/org/newdawn/slick/thingle/internal/slick/ImageBufferWrapper.java
@@ -4,17 +4,35 @@ import org.newdawn.slick.ImageBuffer;
 import org.newdawn.slick.thingle.spi.ThinletImage;
 import org.newdawn.slick.thingle.spi.ThinletImageBuffer;
 
+/**
+ * A wrapped round Slick image buffers to support the graident creation
+ * for Thinlet
+ * 
+ * @author kevin
+ */
 public class ImageBufferWrapper implements ThinletImageBuffer {
+	/** The buffer wrapped */
 	private ImageBuffer buffer;
 	
+	/**
+	 * Create a new wrapper round a slick image buffer
+	 * 
+	 * @param buffer The buffer to be wrapped
+	 */
 	public ImageBufferWrapper(ImageBuffer buffer) {
 		this.buffer = buffer;
 	}
 	
+	/**
+	 * @see org.newdawn.slick.thingle.spi.ThinletImageBuffer#getImage()
+	 */
 	public ThinletImage getImage() {
 		return new ImageWrapper(buffer.getImage());
 	}
 
+	/**
+	 * @see org.newdawn.slick.thingle.spi.ThinletImageBuffer#setRGBA(int, int, int, int, int, int)
+	 */
 	public void setRGBA(int x, int y, int r, int g, int b, int a) {
 		buffer.setRGBA(x, y, r, g, b, a);
 	}
