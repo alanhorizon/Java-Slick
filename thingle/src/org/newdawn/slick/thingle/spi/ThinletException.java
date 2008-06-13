dd6939c6b8598cbaac799f7ef062885679434d78
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2008-06-13 16:10:59 +0000

    Added javadoc

--- thingle/src/org/newdawn/slick/thingle/spi/ThinletException.java
+++ thingle/src/org/newdawn/slick/thingle/spi/ThinletException.java
@@ -1,15 +1,37 @@
 package org.newdawn.slick.thingle.spi;
 
+/**
+ * A wrapper exception to keep implementation seperated from 
+ * framework.
+ * 
+ * @author kevin
+ */
 public class ThinletException extends Exception {
 
+	/**
+	 * Create a new exception
+	 * 
+	 * @param msg The detail message
+	 */
 	public ThinletException(String msg) {
 		super(msg);
 	}
-	
+
+	/**
+	 * Create a new exception
+	 * 
+	 * @param cause The exception causing this exception to be thrown
+	 */
 	public ThinletException(Throwable cause) {
 		super(cause);
 	}
-	
+
+	/**
+	 * Create a new exception
+	 * 
+	 * @param msg The detail message
+	 * @param cause The exception causing this exception to be thrown
+	 */
 	public ThinletException(String msg, Throwable cause) {
 		super(msg, cause);
 	}
