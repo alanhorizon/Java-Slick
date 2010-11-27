f62bd08ed80edd3b3c09d9dd98634013fb255861
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2010-11-27 13:44:10 +0000

    Add the bootstrap activity

--- Slick-Android-Test/src/org/newdawn/slick/android/SlickTestActivity.java
+++ Slick-Android-Test/src/org/newdawn/slick/android/SlickTestActivity.java
@@ -1,34 +1,16 @@
 package org.newdawn.slick.android;
 
-import org.newdawn.slick.AndroidResourceLocation;
-import org.newdawn.slick.GDXGameContainer;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.opengl.renderer.Renderer;
-import org.newdawn.slick.renderer.GDXRenderer;
+import org.newdawn.slick.SlickActivity;
 import org.newdawn.slick.tests.TestGDXContainer;
-import org.newdawn.slick.util.ResourceLoader;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-
-public class SlickTestActivity extends AndroidApplication {
+public class SlickTestActivity extends SlickActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        try {
-			Renderer.setRenderer(new GDXRenderer());
-			Renderer.setLineStripRenderer(Renderer.QUAD_BASED_LINE_STRIP_RENDERER);
-			
-			ResourceLoader.removeAllResourceLocations();
-			ResourceLoader.addResourceLocation(new AndroidResourceLocation(getAssets()));
-        	GDXGameContainer container = new GDXGameContainer(new TestGDXContainer(), 640, 320);        
-        	initialize(container, false);
-        } catch (SlickException e) {
-        	Log.e("SLICK", "Failed to create container", e);
-        }
+        start(new TestGDXContainer(), 640, 320);
     }
 }
